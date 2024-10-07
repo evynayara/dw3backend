c4ea2d5c4acb5b954ab8a9ec355fe7a2f54ec213
Evelyn <evenay99@gmail.com>
2024-10-07 07:57:39 -0300

    Iniciando rep

---
+++ routes/router.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const routerApp = express.Router();
+
+const appAlunos = require("../apps/alunos/controller/ctlAlunos");
+const appLogin = require("../apps/login/controller/ctlLogin");
+
+// middleware that is specific to this router
+routerApp.use((req, res, next) => {
+ next();
+});
+
+routerApp.get("/", (req, res) => {
+ res.send("Olá mundo!");
+});
+
+//Rotas de Alunos
+routerApp.get("/getAllAlunos", appAlunos.getAllAlunos);
+routerApp.post("/getAlunoByID", appLogin.AutenticaJWT, appAlunos.getAlunoByID);
+routerApp.post("/insertAlunos", appLogin.AutenticaJWT, appAlunos.insertAlunos);
+routerApp.post("/updateAlunos", appAlunos.updateAlunos);
+routerApp.post("/DeleteAlunos", appAlunos.DeleteAlunos);
+
+//Rotas de Cursos
+
+// Rota Login
+routerApp.post("/Login", appLogin.Login);
+routerApp.post("/Logout", appLogin.Logout);
+
+module.exports = routerApp;
